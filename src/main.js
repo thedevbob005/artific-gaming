f87f23e8b2d0a74557f0e76246060ba8b9c8a599
Sayantan Bakshi <thedevbob005@gmail.com>
2020-08-11 17:43:26 +0530

    glows now moving & switch to vue2 pwa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import './index.css'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
